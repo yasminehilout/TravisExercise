46b2c9a5113af92291f635c08ce6cf52b7f71e96
Peter Chen <peterc@Peters-MacBook-Pro.local>
2021-02-05 18:11:10 -0500

    added an error by commenting out variable declaration

--- src/Demo.java
+++ src/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
     
 
     // discount strategy
-    int selectedStrat = Integer.parseInt(argv[0]);
+    // int selectedStrat = Integer.parseInt(argv[0]);
 
     // discount amount
     float amt = Float.parseFloat(argv[1]);
